a77a9e7a1a49fcf43faaad3c9fcc0a4c81d80dc1
Pierre Gaudin <pierre.gaudin@etu.unice.fr>
2024-09-24 09:30:30 +0200

    td2 fix size map and pos

--- TD2/scripts/td2scripts1.js
+++ TD2/scripts/td2scripts1.js
@@ -1,28 +1,24 @@
 navigator.geolocation.getCurrentPosition(showMap);
 
 
-// Fonction appelée en cas de succès de la géolocalisation
 function showMap(position) {
-    // Coordonnées de l'utilisateur
     const userLatitude = position.coords.latitude;
     const userLongitude = position.coords.longitude;
 
-    // Coordonnées de Nice (centre ville)
+    document.getElementById('current-lat').textContent = `Latitude : ${userLatitude}`;
+    document.getElementById('current-lon').textContent = `Longitude : ${userLongitude}`;
+
     const niceCoords = [43.7102, 7.2620];
 
-    // Initialiser la carte centrée sur la position de l'utilisateur
     const map = L.map('map').setView([userLatitude, userLongitude], 13);
 
-    // Ajouter le layer OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors'
     }).addTo(map);
 
-    // Ajouter un marqueur à la position de l'utilisateur
     const userMarker = L.marker([userLatitude, userLongitude]).addTo(map);
     userMarker.bindPopup("<b>Vous êtes ici</b>").openPopup();
 
-    // Ajouter un marqueur à Nice
     const niceMarker = L.marker(niceCoords).addTo(map);
     niceMarker.bindPopup("<b>Nice - Centre Ville</b>").openPopup();
 }
