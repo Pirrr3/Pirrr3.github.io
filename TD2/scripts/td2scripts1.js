d50686621e4668d3f48e3e05dc59d59fb49d06e6
Pierre Gaudin <pierre.gaudin@etu.unice.fr>
2024-09-24 09:26:33 +0200

    td2 mini fix

--- TD2/scripts/td2scripts1.js
+++ TD2/scripts/td2scripts1.js
@@ -1,9 +1,5 @@
-// Vérification si la géolocalisation est disponible
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showMap, showError);
-} else {
-    alert("La géolocalisation n'est pas supportée par votre navigateur.");
-}
+navigator.geolocation.getCurrentPosition(showMap);
+
 
 // Fonction appelée en cas de succès de la géolocalisation
 function showMap(position) {
@@ -29,22 +25,4 @@ function showMap(position) {
     // Ajouter un marqueur à Nice
     const niceMarker = L.marker(niceCoords).addTo(map);
     niceMarker.bindPopup("<b>Nice - Centre Ville</b>").openPopup();
-}
-
-// Fonction appelée en cas d'erreur de géolocalisation
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            alert("L'utilisateur a refusé la demande de géolocalisation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Les informations de localisation sont indisponibles.");
-            break;
-        case error.TIMEOUT:
-            alert("La demande de localisation a expiré.");
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("Une erreur inconnue s'est produite.");
-            break;
-    }
 }
