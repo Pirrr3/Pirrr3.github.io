3c2b1cba4da461303b7915dc05410b99dd386fbd
Pierre Gaudin <pierre.gaudin@etu.unice.fr>
2024-09-24 09:10:13 +0200

    td2 affiche map

--- TD2/scripts/scripts1.js
+++ TD2/scripts/scripts1.js
@@ -33,3 +33,27 @@ document.addEventListener('DOMContentLoaded', function() {
     getCurrentPosition();
     watchPosition();
 });
+
+
+function showMap(position) {
+    const userLatitude = position.coords.latitude;
+    const userLongitude = position.coords.longitude;
+
+    const niceCoords = [43.7102, 7.2620];
+
+    const map = L.map('map').setView([userLatitude, userLongitude], 13);
+
+    // Ajouter le layer OpenStreetMap
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // Ajouter un marqueur à la position de l'utilisateur
+    const userMarker = L.marker([userLatitude, userLongitude]).addTo(map);
+    userMarker.bindPopup("<b>Vous êtes ici</b>").openPopup();
+
+    // Ajouter un marqueur à Nice
+    const niceMarker = L.marker(niceCoords).addTo(map);
+    niceMarker.bindPopup("<b>Nice - Centre Ville</b>").openPopup();
+}
